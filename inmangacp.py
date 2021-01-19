4c1b86c323676e60ae7b96f13547d17d4f5a5231
obskur123 <tomasxd768@outlook.com>
2021-01-18 21:58:03 -0300

    agregue tqdm para mostrar barras de progreso y cambie la logica al dercargar

--- inmangacp.py
+++ inmangacp.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from tqdm import tqdm
 import requests
 import logging
 import time
@@ -34,17 +35,29 @@ try:
 
     logging.info(f'Numero de paginas: {number_of_pages}')
 
-    requests_array = []
-
     folder_name = driver.find_element_by_xpath('/html/body/div/section/div/div/div[5]/h1').text
     
     if ':' in folder_name:
         
         folder_name = folder_name.replace(':','-')
+        
+                
+    if not os.path.exists('.\\Mangas'):
+                
+        os.mkdir('.\\Mangas')
+            
+        os.mkdir(f'.\\Mangas\\{folder_name}')
+            
+        logging.info(f'Creando directorio: {folder_name}.')
+            
+    else:
+            
+        os.mkdir(f'.\\Mangas\\{folder_name}')
+        logging.info(f'Creando directorio: {folder_name}.')
     
     
     for i in range(1, number_of_pages+1):
-    
+        
         logging.info(f'Descargando pag num {i}.')
         
         img_web_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div/section/div/div/div[2]/div[2]/div[1]/a[{i}]/img')))
@@ -52,9 +65,22 @@ try:
         url_image = img_web_element.get_attribute('src')
 
         r = requests.get(url_image)
-
-        requests_array.append(r)
         
+        total_size_in_bytes = int(r.headers.get('content-length', 0))
+        
+        block_size = 1024
+        
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        
+        logging.info(f'Descargando pag {i}.')
+        
+        if r.status_code is 200:
+            with open(f'.\\Mangas\\{folder_name}\\page{i}.png', 'wb') as f:
+                for chunk in r.iter_content(block_size):
+                    progress_bar.update(len(chunk))
+                    f.write(chunk) 
+
+
         next_button = driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/div[1]/div/div/div[5]/div/button[2]').click()
         
     
@@ -69,30 +95,7 @@ except  NoSuchElementException as e:
     driver.close()
     sys.exit('Solo acepto URL\'s de capitulos de inmanga.')
 
-
-
-if not os.path.exists('.\\Mangas'):
-        
-    os.mkdir('.\\Mangas')
     
-    os.mkdir(f'.\\Mangas\\{folder_name}')
-    
-    logging.info(f'Creando directorio: {folder_name}.')
-    
-else:
-    
-    os.mkdir(f'.\\Mangas\\{folder_name}')
-    logging.info(f'Creando directorio: {folder_name}.')
-
-
-page = 1
-for request in requests_array:
-    logging.info(f'Construyendo pag num {page}')
-    if request.status_code is 200:
-        with open(f'.\\Mangas\\{folder_name}\\page{page}.png', 'wb') as f:
-            for chunk in request:
-                f.write(chunk) 
-    page+=1
     
 logging.info(f'Capitulo guardado en .\\Mangas\\{folder_name}.')
 driver.close()
